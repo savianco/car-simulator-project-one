e8037710e1e6a39f8ae1c26c521498261f1cf910
Saturnino Villaverde <savianco@hotmail.com>
2017-02-03 12:09:36 +0100

    Done
    Done

--- js/carSimulator.js
+++ js/carSimulator.js
@@ -1,106 +1,102 @@
-
-//////// funcion para explotar en contacto con los bordes ////////
+////// Funciones de control LIFES, WALL CRASHES and EXPLOSIONS /////
 var lifes = 2;
 var crashHacePoco = false;
 function explosion(){
     var explosionSound = new Audio ("sounds/explosion.mp3");
         explosionSound.play();
-  $("#car").addClass("crashed");
+        $("#car").addClass("crashed");
     
     var life = $(".container-lifes li")[lifes];
-    if(life !== undefined && crashHacePoco===false){
+    if (life !== undefined && crashHacePoco === false) {
         life.style.display = "none";
         lifes--;
-        crashHacePoco=true;
-        setTimeout(function(){
-           crashHacePoco=false; 
-        },1000);
-    }else if (life === undefined){msn()};
-    setTimeout(function(){
+        crashHacePoco = true;
+        setTimeout(function() {
+        crashHacePoco = false; 
+    },1000);
+        
+    } else if (life === undefined){msn()};
+        setTimeout(function() {
         $("#car").removeClass("crashed");
     },2000); 
 }
 
 
-
-
-
-
-
-////////// seleccionamos coche  //////////
+////////// Declaración variables de posiciones coche y pista //////////
 var car = document.getElementById("car");
 var board = $("#board");
 var grassIn = $("#grass-in");
+var carPrevX;
+var carPrevY;
+var carCurrentX;
+var carCurrentY;
 
 
-   var carPrevX;
-   var carPrevY;
-    var carCurrentX;
-    var carCurrentY;
-/// asignamos teclas, movimientos, direcciones, area de pista ///
-window.addEventListener("keydown",function(e){
+///// Asignacion de teclas movimientos, direcciones, area de pista //////
+window.addEventListener("keydown",function(e) {
     carPrevX = $("#car").css("left");
     carPrevY = $("#car").css("top");
-   var car = $("#car");
-   var left = parseInt(car.css("left"));
-   var boardWidth = parseInt(board.css("width"));
-   var top = parseInt(car.css("top"));
-   var boardHeight = parseInt(board.css("height"));
-    var mov = 15;
-     switch(e.keyCode){
-       case 37:  // arrow left
-         if (left > 0 && allowMoves) {
-            $("#car").css("left","-="+mov+"");
-            $("#car").removeClass("down");
-            $("#car").removeClass("right");
-            $("#car").removeClass("up");
-            $("#car").addClass("left");
-         }  else if(left < 0) {
-              explosion();
-              console.log("choco");
+    var car = $("#car");
+    var left = parseInt(car.css("left"));
+    var boardWidth = parseInt(board.css("width"));
+    var top = parseInt(car.css("top"));
+    var boardHeight = parseInt(board.css("height"));
+    var mov = 15;  /////// CONTROL DE VELOCIDAD ///////
+       switch(e.keyCode){
+         case 37:  //////// arrow LEFT /////
+           if (left > 0 && allowMoves) {
+              $("#car").css("left","-="+mov+"");
+              $("#car").removeClass("down");
+              $("#car").removeClass("right");
+              $("#car").removeClass("up");
+              $("#car").addClass("left");
+           } else if(left < 0) {
+               explosion();
+               console.log("crash");
          };
-            break;
+             break;
             
-       case 39:  // arrow right
-         if (left + parseInt(car.css("width")) < boardWidth && allowMoves) {
-            $("#car").css("left","+="+mov+"");
-            $("#car").removeClass("left");
-            $("#car").removeClass("down");
-            $("#car").removeClass("up");
-            $("#car").addClass("right");
-         }  else if(left + parseInt(car.css("width")) > boardWidth) {
-              explosion();
-              console.log("choco");
+         case 39:  ////// arrow RIGHT /////
+           if (left + parseInt(car.css("width")) < boardWidth && allowMoves) {
+              $("#car").css("left","+="+mov+"");
+              $("#car").removeClass("left");
+              $("#car").removeClass("down");
+              $("#car").removeClass("up");
+              $("#car").addClass("right");
+           } else if(left + parseInt(car.css("width")) > boardWidth) {
+               explosion();
+               console.log("crash");
          };  
-            break; 
+             break; 
             
-       case 38: // arrow up
-         if (top > 10 && allowMoves) {
-            $("#car").css("top","-="+mov+"");
-            $("#car").removeClass("left");
-            $("#car").removeClass("right");
-            $("#car").removeClass("down");
-            $("#car").addClass("up");
-         }  else if(top < 10) {
-              explosion();
-              console.log("choco");
+          case 38: ///// arrow UP //////
+            if (top > 10 && allowMoves) {
+               $("#car").css("top","-="+mov+"");
+               $("#car").removeClass("left");
+               $("#car").removeClass("right");
+               $("#car").removeClass("down");
+               $("#car").addClass("up");
+            } else if(top < 10) {
+                explosion();
+                console.log("crash");
          };
-            break;
+              break;
                      
-       case 40: // arrow down
-         if (top + parseInt(car.css("height")) < (boardHeight + 10) && allowMoves) {
-            $("#car").css("top","+="+mov+"");
-            $("#car").removeClass("left");
-            $("#car").removeClass("right");
-            $("#car").removeClass("up");
-            $("#car").addClass("down");        
-         }  else if(top + parseInt(car.css("height")) > boardHeight) {
-              explosion();
-              console.log("choco");
+          case 40: ///// arrow DOWN //////
+            if (top + parseInt(car.css("height")) < (boardHeight + 10) &&          allowMoves) {
+               $("#car").css("top","+="+mov+"");
+               $("#car").removeClass("left");
+               $("#car").removeClass("right");
+               $("#car").removeClass("up");
+               $("#car").addClass("down");        
+            } else if(top + parseInt(car.css("height")) > boardHeight) {
+                explosion();
+                console.log("crash");
          };
-            break;        
+              break;        
      }
+    
     carCurrentX = $("#car").css("left");
     carCurrentY = $("#car").css("top");
- }.bind(this));
+}.bind(this));
 
--- js/timeManager.js
+++ js/timeManager.js
@@ -1,32 +1,39 @@
 console.log("linked");
 
+////// funcion de activacion del PLAY y del TIMER ///////////
 var start = document.getElementById("button-play");
 var allowMoves = false;
 start.onclick = function(){
+    var startSound = new Audio("sounds/startCar.mp3");
+    var gameSound = new Audio("sounds/gameSound.mp3");
+    startSound.play();
+    gameSound.play();
+
+    
     startTimer();
     allowMoves = true;
 };
-
     var i;
-    var restar;
+    var restarInterval;
     var cambiar01;
     var msn;
 
-////// funcion del croometro ////////////////////
+/////// funcion del cronometro iniciado con 1 minuto de juego ////////
 var timer;
-function startTimer(){
+function startTimer() {
      i=59;
-     restar = setInterval(restar,1000);
+     restarInterval = setInterval(restar,1000);
      cambiar01 = setTimeout(cambiar01,1000);
      timer = setTimeout(msn,60000);  
 }
 
-
-function cambiar01(){
+///////// funcion para modificar los minutos del TIMER ///////
+function cambiar01() {
 	document.getElementById('minutes').innerHTML = '00';
 }
 
-function restar(){
+////// funcion restar los segundos del TIMER hasta cero y pintarlo bien ////
+function restar() {
 	document.getElementById('seconds').textContent = i; i--;
     if (i < 0) {
 	   clearInterval(restar);
@@ -35,9 +42,10 @@ function restar(){
   }
 }
 
-
+///////////// funcion para activar el GAME OVER ////////////
 function msn(){
-    document.getElementById("game-over").innerHTML = $("#game-over").css("opacity",1);
+   document.getElementById("game-over").innerHTML = $("#game-over").css("opacity",1);
+    clearInterval(restarInterval);
 }
 
-
+ 
